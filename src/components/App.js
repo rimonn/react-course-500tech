1032317ec946acaa79d4ce1edc0310ca67ec88e9
Nadav Rimon <nadavr@bidalgo.com>
2018-10-03 15:49:59 +0300

    3 ways to bind 'this'

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,9 @@ export default class App extends React.Component {
       count: 0
     };
 
+    // equivalent to: add = () => ...
+    //                sub = () => ...
+    // but more elegant
     //this.add = this.add.bind(this);
     //this.sub = this.sub.bind(this);
   }
@@ -32,6 +35,9 @@ export default class App extends React.Component {
     return (
       <div>
         <div>{count}</div>
+        {/*not recommended because causing the binding to be performed each render*/}
+        {/*<button onClick={() => this.add.bind(this)}>add</button>*/}
+        
         <button onClick={this.add}>add</button>
         <button onClick={this.sub}>sub</button>
       </div>
