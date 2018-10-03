c81745c704c29185226141f9f0a66529b51dec4a
Nadav Rimon <nadavr@bidalgo.com>
2018-10-03 13:20:30 +0300

    use css from styled-components and extend styled component

--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const GroceryItem = ({itemName, onClickHandler}) => (
-  <Container>
+  <Container isDisabled={true}>
     <div onClick={() => onClickHandler(itemName)}>{itemName}</div>
   </Container>
 );
@@ -11,6 +11,14 @@ export const GroceryListItem = props => (
   props.itemName && <li><GroceryItem {...props} /></li>
 );
 
+// use css from styled-components
 const Container = styled.div`
   background-color: cornflowerblue;
+  ${({isDisabled}) => isDisabled && css`
+    background-color: red;
+  `}
+`;
+
+// extend the Container styled component
+const BigContainer = Container.extend`
 `;
