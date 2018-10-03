6af1ac7eb8181ebabfaacc52638ac6cefd4d34a2
Nadav Rimon <nadavr@bidalgo.com>
2018-10-03 10:33:47 +0300

    React.createElement

---
+++ NOTES.md
@@ -0,0 +1,10 @@
+# Notes from course
+
+* https://babeljs.io/repl
+* virtual-dom
+* Each child in an array or iterator should have a unique "key" prop
+* stateless component and state component
+
+
+### Steps and branches
+1. `react-16` branch
--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const groceries = [
+  'milk',
+  'soy',
+  'apple'
+];
+
 ReactDOM.render(
-  React.createElement('h3', null, 'hello React & Redux course'),
+  React.createElement('div', null, [
+    React.createElement('h3', null, 'my list'),
+    React.createElement('ul', null, [
+      groceries.map(singleItem => React.createElement('li', null, singleItem))
+      
+      // Equivalent to:
+      // React.createElement('li', null, 'soy'),
+      // React.createElement('li', null, 'apple'),
+      // React.createElement('li', null, 'apple'),
+    ])
+  ]),
   document.getElementById('root')
 );
 registerServiceWorker();
